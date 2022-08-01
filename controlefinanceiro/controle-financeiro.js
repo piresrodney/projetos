a08489754961a08722789afc50c0f8405190483c
piresrodney <piresrodney@gmail.com>
2022-08-01 14:23:28 -0300

    Regras controle financeiro

--- controlefinanceiro/controle-financeiro.js
+++ controlefinanceiro/controle-financeiro.js
@@ -1,4 +1,7 @@
 const campoValor = document.querySelector('input#inputvalor');
+const campoDescricao = document.querySelector('input#inputdescricao');
+const campoData = document.querySelector('input#inputdata');
+const campoTipo = document.querySelector('select#tipo')
 
 const controleFinanceiro = {
     dadosFinanceiros: [
@@ -6,14 +9,69 @@ const controleFinanceiro = {
             valor: 0,
             descricao: '',
             dataLancamento: new Date,
-            tipo: ''
+            tipo: -1
         }            
     ]
 } 
 
+const dadosValidos = {
+    valido: false,
+    mensagem: ''
+}
+
 const validacoesCampos = {
     validarCampoValor() {
-        return campoValor.value > 0;
+        dadosValidos.valido = campoValor.value > 0;
+        dadosValidos.mensagem = '';
+
+        if (!dadosValidos.valido) {
+           dadosValidos.mensagem = 'Informe um valor válido e/ou maior do que zero.'; 
+        }        
+    },
+    validarCampoDescricao() {
+        dadosValidos.valido = campoDescricao.value.trim() != '';
+        dadosValidos.mensagem = '';
+
+        if (!dadosValidos.valido) {
+            dadosValidos.mensagem = 'Informe uma descrição para o lançamento.'; 
+        }      
+    },
+    validarCampoData() {
+        dadosValidos.valido = campoData.value != 0;
+        dadosValidos.mensagem = '';
+
+        if (!dadosValidos.valido) {
+            dadosValidos.mensagem = 'Informe uma data para o lançamento.'; 
+        }  
+    }
+}
+
+const validarCampos = {
+    validarCamposDaTela() {
+        validacoesCampos.validarCampoValor();
+        let resultado = dadosValidos.valido;
+
+        if (!resultado) {
+            campoValor.focus();
+        } else {
+            validacoesCampos.validarCampoDescricao()
+            resultado = dadosValidos.valido;
+
+            if (!resultado) {
+                campoDescricao.focus();
+            } else {
+                validacoesCampos.validarCampoData()
+                resultado = dadosValidos.valido;
+
+                if (!resultado) {
+                    campoData.focus();
+                }
+            }
+        }
+
+        if (!resultado) {
+            alert(dadosValidos.mensagem);
+        }
     }
 }
 
@@ -22,24 +80,19 @@ window.onload = function () {
     campoValor.focus();
 }
 
-function gravarDadoFinanceiro() {
-    if (!validacoesCampos.validarCampoValor()) {
-        alert('Informe um valor válido e/ou maior do que zero.');
-        campoValor.focus();
-    }
-    else {
-        alert(campoValor.value);
-    }
+function gravarDadoFinanceiro() { 
+    validarCampos.validarCamposDaTela();
     
-    // push é usado para incluir informações no array
-    /* 
-    controleFinanceiro
-        .dadosFinanceiros
-        .push({ valor: 1.99, descricao: 'Teste de descricao', dataLancamento: new Date, tipo: 'E' }); 
-
-   */     
+    if (dadosValidos.valido) {    
+        // push é usado para incluir informações no array        
+        controleFinanceiro
+            .dadosFinanceiros
+            .push({ valor: campoValor.value, 
+                    descricao: campoDescricao.value.trim(), 
+                    dataLancamento: campoData.value, 
+                    tipo: campoTipo.selectedIndex });  
+                    
+        alert('Incluído com sucesso.');                               
+        //alert(controleFinanceiro.dadosFinanceiros);
+    }     
 }
-
-//gravarDadoFinanceiro();
-
-//console.log(controleFinanceiro.dadosFinanceiros[1].valor);
